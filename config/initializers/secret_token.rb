b5a2cd90eb632b2c1c13fef37af27bab39764ddd
Jinesh Patel <caprikorn_ji@yahoo.co.in>
2012-08-18 12:41:21 +0530

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jinesh::Application.config.secret_token = 'd8d23407336cc758013ea13064023e5f6502d456021c690f1e83ec98030859ac2ee406c2632504e8ada43ee8880893be189ed2eaa40c7bc52fed6533dc441a80'
